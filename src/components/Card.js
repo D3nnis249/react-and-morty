90d0467a959c29d66ce0aa97b8e102970aa745df
D3nnis249 <100355993+D3nnis249@users.noreply.github.com>
2022-04-05 14:14:03 +0200

    Revert "Revert "add style with styled-components""

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
 const Card = () => {
   return (
-    <div>
-      <img className="Cards" src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"></img>
-        <p>Morty Smith</p>
-    </div>
+      <Wrapper>
+    <StyledCard>
+      <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"></img>
+        <h2>Morty Smith</h2>
+    </StyledCard>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+display: flex;
+justify-content: center;
+
+`;
+
+
+const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: auto;
+  border: 2px solid black;
+  
+`;
+
 
 export default Card;
